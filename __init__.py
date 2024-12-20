4eb6e63bd4676bc8d9d5f5fa05b7868666409551
YONGGI HONG <39263586+HongYongGi@users.noreply.github.com>
2024-12-20 18:06:35 +0900

    Refactor __init__.py to remove unused evaluation functions and streamline exports. Added new conversion functions for NIfTI file handling.

--- __init__.py
+++ __init__.py
@@ -1,16 +1,9 @@
 __all__ = [
-    'confusion_matrix','dice',
-    'sensitivity','specificity','precision', 'recall',
-    'accuracy','f1_score','fpr',
-    'ssim','psnr','mse','mae','rmse', 
-    
+    'dcm2nii', 'nii2niigz', 'niigz2nii', 'nnunet_preprocess',
 
 
 ]
 
 
-
-
-from .evaluation import confusion_matrix, dice, sensitivity, specificity, precision, recall, accuracy, f1_score, fpr, ssim, psnr, mse, mae, rmse
 from .convert import dcm2nii, nii2niigz, niigz2nii, nnunet_preprocess
 from .convert import load_nii, save_nii, load_dcm
